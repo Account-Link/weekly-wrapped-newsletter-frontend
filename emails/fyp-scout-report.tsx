2b37c76bfa1790ea4dc0adb878efa149bc7c78ea
Hx <zhx2gang@gmail.com>
2026-02-02 16:43:47 +0800

    找到原因 是 vpn 的原因

--- emails/fyp-scout-report.tsx
+++ emails/fyp-scout-report.tsx
@@ -730,7 +730,6 @@ export function FypScoutReportEmail({ data }: FypScoutReportEmailProps) {
                     <Img
                       src={getImgUrl(CatSleep.src)}
                       width="160"
-                      height="160"
                       className="mobile-rabbit-img"
                     />
                   </Column>
